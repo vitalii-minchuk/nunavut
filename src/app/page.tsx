eb71e3f3649afadc7d4c769500101eb808499ad2
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-05-06 23:20:39 +0300

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  return (
-    <main>
-      <Button>test</Button>
-    </main>
-  );
+  return <main>hello</main>;
 }
