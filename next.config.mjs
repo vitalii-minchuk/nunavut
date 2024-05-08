09d34c41ebd37ffa50d59594d9e79e3a57c26d35
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-05-08 22:29:24 +0300

    add cld image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
--- src/components/media-gallery/media-gallery-item/index.tsx
+++ src/components/media-gallery/media-gallery-item/index.tsx
@@ -1,6 +1,8 @@
-import { ICloudinaryResource } from '@/types';
-import Image from 'next/image';
+'use client';
+
 import Link from 'next/link';
+import { ICloudinaryResource } from '@/types';
+import { CldImage } from 'next-cloudinary';
 
 interface Props {
   resource: ICloudinaryResource;
@@ -10,12 +12,14 @@ export const MediaGalleryItem = (props: Props) => {
   const { resource } = props;
   return (
     <Link href={`/${resource.public_id}`}>
-      <Image
-        src={resource.secure_url}
-        alt="resource"
-        width={resource.width}
-        height={resource.height}
-      />
+      <div className="w-[200px]">
+        <CldImage
+          src={resource.public_id}
+          alt="resource"
+          width={resource.width}
+          height={resource.height}
+        />
+      </div>
     </Link>
   );
 };
